d067c466a1e39c0207b0f641b4b22bda33a045ec
DHANIAK <rajuredhanika@gmail.com>
2026-05-25 21:01:02 +0530

    Time: 1686 ms (7.69%), Space: 26.4 MB (93.53%) - LeetHub - Time: O(?), Space: O(?)

---
+++ 0189-rotate-array/0189-rotate-array.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n=len(nums)
+        rotations=k%n
+        for _ in range(rotations):
+
+            e=nums.pop()
+            nums.insert(0,e)
+        
+
+# Synced seamlessly with LeetHub Pro
+# Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+# Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
